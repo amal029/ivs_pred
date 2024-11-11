2707cbddb2f3c8436e3c7abce54d9d0304e44f3c
pbriesch <pbriesch@live.com>
2024-11-11 14:11:53 +1300

    non-linear models for ssvi parameter prediction

--- non_linear_models.py
+++ non_linear_models.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import svm
 import keras 
 from keras.layers import LSTM, Dense
+from sklearn.ensemble import RandomForestRegressor
 
 
 
@@ -15,22 +16,70 @@ class SVM():
     def predict(self, X):
         return self.model.predict(X)
 
+class RF():
+    def __init__(self, n_estimators=1000):
+        self.model = RandomForestRegressor(n_estimators=n_estimators)
+    
+    def fit(self, X, y):
+        self.model.fit(X, y)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+
+class DNN():
+    def __init__(self, input_shape, output_shape):
+        self.isfit = False
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.Dense(units=64, input_shape=input_shape, activation='relu'))
+        self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.Dense(units=128, activation='relu'))
+        self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.Dense(units=128, activation='relu'))
+        self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.Dense(units=64, activation='relu'))
+        self.model.add(Dense(output_shape, activation='linear'))
+    
+    def compile(self, loss='mean_squared_error', optimizer='adam'):
+        self.model.compile(loss=loss, optimizer=optimizer)
+    
+    def fit(self, X, y, epochs=500, batch_size=32, verbose=1):
+        # Early stopping call back
+        early_stopping = keras.callbacks.EarlyStopping(monitor='loss', patience=10)
+        # Learning rate callback
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=0.0001)
+        self.model.fit(X, y, epochs=epochs, batch_size=batch_size, verbose=verbose, callbacks=[early_stopping, reduce_lr])
+        self.isfit = True
+    
+    def check_is_fitted(self):
+        return self.isfit
+    
+    def predict(self, X):
+        return self.model.predict(X)
 class LSTM():
     def __init__(self, input_shape, output_shape):
         self.isfit = False
         self.model = keras.models.Sequential()
-        self.model.add(keras.layers.LSTM(units=125, input_shape=input_shape, return_sequences=True, activation='relu'))
+        self.model.add(keras.layers.LSTM(units=128, input_shape=input_shape, return_sequences=True, activation='tanh'))
         self.model.add(keras.layers.Dropout(0.2))
-        self.model.add(keras.layers.LSTM(units=125, return_sequences=True, activation='relu'))
+        self.model.add(keras.layers.LSTM(units=128, return_sequences=True, activation='tanh'))
         self.model.add(keras.layers.Dropout(0.2))
-        self.model.add(keras.layers.LSTM(units=125, return_sequences=False, activation='relu'))
+        self.model.add(keras.layers.LSTM(units=128, return_sequences=True, activation='tanh'))
+        self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.LSTM(units=128, return_sequences=True, activation='tanh'))
+        self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.LSTM(units=128, return_sequences=True, activation='tanh'))
+        self.model.add(keras.layers.LSTM(units=32, return_sequences=False, activation='tanh'))
         self.model.add(Dense(output_shape))
     
     def compile(self, loss='mean_squared_error', optimizer='adam'):
         self.model.compile(loss=loss, optimizer=optimizer)
     
-    def fit(self, X, y, epochs=100, batch_size=32):
-        self.model.fit(X, y, epochs=epochs, batch_size=batch_size)
+    def fit(self, X, y, epochs=200, batch_size=16, verbose=1):
+        # Early stopping call back
+        early_stopping = keras.callbacks.EarlyStopping(monitor='loss', patience=10)
+        # Learning rate callback
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=0.0001)
+        self.model.fit(X, y, epochs=epochs, batch_size=batch_size, verbose=verbose, callbacks=[reduce_lr, early_stopping])
         self.isfit = True
     
     def check_is_fitted(self):
